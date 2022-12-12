cc8c3e2c635f03d877be876027fab5f5b5d408af
ShinQQ <denis110402@gmail.com>
2022-12-12 21:35:08 +0200

    Add project files.

---
+++ WeatherAlertsBot/Program.cs
@@ -0,0 +1,54 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Microsoft.Extensions.Configuration;
+using Telegram.Bot.Exceptions;
+
+var configuration = new ConfigurationBuilder()
+                 .AddJsonFile($"appsettings.json", true, true).Build();
+
+var botClient = new TelegramBotClient(configuration["BotAccessToken"]);
+
+using CancellationTokenSource cansellationTokenSource = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    HandleUpdateAsync,
+    HandlePollingErrorAsync,
+    receiverOptions,
+    cansellationTokenSource.Token
+    );
+
+Console.ReadLine();
+
+cansellationTokenSource.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    var userMessageText = update.Message.Text;
+
+    string message = "Something went wrong";
+
+    if (userMessageText.ToLower().Contains("weather")) message = "I`ll send you the weather!";
+    else if (userMessageText.ToLower().Contains("something")) message = "something";
+
+    await botClient.SendTextMessageAsync(update.Message.Chat.Id, message, cancellationToken: cancellationToken);
+}
+
+async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    await Task.CompletedTask;
+}
