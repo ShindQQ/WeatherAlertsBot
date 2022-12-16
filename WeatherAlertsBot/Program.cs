94811d8b37a41e410bddc20c0b1ebdbc33bdadcc
ShinQQ <denis110402@gmail.com>
2022-12-16 23:21:11 +0200

    Fixed nesting Code clean up Thanks DTEK

--- WeatherAlertsBot/Program.cs
+++ WeatherAlertsBot/Program.cs
@@ -4,7 +4,6 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
 using WeatherAlertsBot.OpenWeatherAPI;
 using WeatherAlertsBot.TelegramBotHandlers;
 
--- WeatherAlertsBot/TelegramBotHandlers/UpdateHandler.cs
+++ WeatherAlertsBot/TelegramBotHandlers/UpdateHandler.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using System.Threading;
-using Telegram.Bot;
+﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using WeatherAlertsBot.OpenWeatherAPI;
@@ -30,34 +28,45 @@ public sealed class UpdateHandler
         {
             var userMessageText = Update.Message.Text;
 
-            if (userMessageText.ToLower().Contains("/start"))
-            {
-                await BotClient.SendTextMessageAsync(Update.Message.Chat.Id,
-                    "`Hello!\nTo receive weather by city name send me the message in format: weather [city_name]!`",
-                    ParseMode.MarkdownV2, cancellationToken: CancellationToken);
-            }
-            else if (userMessageText.ToLower().Contains("weather"))
-            {
-                var weatherResponseForUser = await weatherHandler.SendWeatherByUserMessageAsync(userMessageText);
-                var errorMessage = weatherResponseForUser.ErrorMessage;
-
-                if (string.IsNullOrEmpty(errorMessage))
-                {
-                    var location = await BotClient.SendLocationAsync(Update.Message.Chat.Id,
-                       weatherResponseForUser.Lattitude, weatherResponseForUser.Longitude,
-                       cancellationToken: CancellationToken);
-                    await BotClient.SendTextMessageAsync(location.Chat.Id,
-                       @$"`Current weather in {weatherResponseForUser.CityName} is {weatherResponseForUser.Temperature} °C, " +
-                       "feels like {weatherResponseForUser.FeelsLike} °C.`",
-                       ParseMode.MarkdownV2, cancellationToken: CancellationToken, replyToMessageId: location.MessageId);
-                    return;
-                }
-
-                await BotClient.SendTextMessageAsync(Update.Message.Chat.Id,
-                       errorMessage, ParseMode.MarkdownV2, cancellationToken: CancellationToken);
-            };
+            await HandleStartMessageAsync(userMessageText);
+            await HandleWeatherMessageAsync(userMessageText);
         }
     }
+
+    public async Task HandleStartMessageAsync(string userMessageText)
+    {
+        if (userMessageText.ToLower().Contains("/start"))
+        {
+            await BotClient.SendTextMessageAsync(Update.Message.Chat.Id,
+                "`Hello!\nTo receive weather by city name send me the message in format: weather [city_name]!`",
+                ParseMode.MarkdownV2, cancellationToken: CancellationToken);
+        }
+    }
+
+    public async Task HandleWeatherMessageAsync(string userMessageText)
+    {
+        if (userMessageText.ToLower().Contains("weather"))
+        {
+            var weatherResponseForUser = await weatherHandler.SendWeatherByUserMessageAsync(userMessageText);
+            var errorMessage = weatherResponseForUser.ErrorMessage;
+
+            if (string.IsNullOrEmpty(errorMessage))
+            {
+                var location = await BotClient.SendLocationAsync(Update.Message.Chat.Id,
+                   weatherResponseForUser.Lattitude, weatherResponseForUser.Longitude,
+                   cancellationToken: CancellationToken);
+                await BotClient.SendTextMessageAsync(location.Chat.Id,
+                   @$"`Current weather in {weatherResponseForUser.CityName} is {weatherResponseForUser.Temperature} °C, " +
+                   "feels like {weatherResponseForUser.FeelsLike} °C.`",
+                   ParseMode.MarkdownV2, cancellationToken: CancellationToken, replyToMessageId: location.MessageId);
+                return;
+            }
+
+            await BotClient.SendTextMessageAsync(Update.Message.Chat.Id,
+                   errorMessage, ParseMode.MarkdownV2, cancellationToken: CancellationToken);
+        };
+    }
+
     public async Task HandleMessageLocationAsync()
     {
         if (Update.Message.Location != null)
